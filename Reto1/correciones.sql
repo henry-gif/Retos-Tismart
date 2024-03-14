255e566a0703b22442fd554b4d53663f8e0d2205
henry-gif <hlozadaq1@upao.edu.pe>
2024-03-14 15:57:23 -0500

    atualizacion

--- Reto1/correciones.sql
+++ Reto1/correciones.sql
@@ -15,14 +15,10 @@ CREATE OR REPLACE PACKAGE HospitalPackage AS
     );
 
     PROCEDURE SP_HOSPITAL_ACTUALIZAR (
-        idHospital IN INTEGER,
-        nombre_hospital IN VARCHAR,
-        antiguedad IN INTEGER,
-        area IN DECIMAL,
-        idDistrito IN INTEGER,
-        idSede IN INTEGER,
-        idGerente IN INTEGER,
-        idCondicion IN INTEGER
+    idHospital IN INTEGER,
+    nuevo_nombre IN VARCHAR,
+    nueva_antiguedad IN INTEGER,
+    nueva_area IN DECIMAL
     );
 
     PROCEDURE SP_HOSPITAL_ELIMINAR (
@@ -110,39 +106,33 @@ CREATE OR REPLACE PACKAGE BODY HospitalPackage AS
     END SP_HOSPITAL_REGISTRAR;
 
     PROCEDURE SP_HOSPITAL_ACTUALIZAR (
-        idHospital IN INTEGER,
-        nombre_hospital IN VARCHAR,
-        antiguedad IN INTEGER,
-        area IN DECIMAL,
-        idDistrito IN INTEGER,
-        idSede IN INTEGER,
-        idGerente IN INTEGER,
-        idCondicion IN INTEGER
+    idHospital IN INTEGER,
+    nuevo_nombre IN VARCHAR,
+    nueva_antiguedad IN INTEGER,
+    nueva_area IN DECIMAL
     ) IS
     BEGIN
-        IF REGEXP_LIKE(nombre_hospital, '^[A-Za-z\sáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ,.?!¡¿]+$') = TRUE THEN
-            DBMS_OUTPUT.PUT_LINE('El nombre del hospital no cumple con el patrón.');
-        ELSE
-            DBMS_OUTPUT.PUT_LINE('El nombre del hospital cumple con el patrón.');
-        END IF;
+    -- Verificar que el nombre del hospital solo contenga caracteres alfabéticos
+    IF REGEXP_LIKE(nuevo_nombre, '^[A-Za-z\sáéíóúÁÉÍÓÚäëïöüÄËÏÖÜñÑ,.?!¡¿]+$') = FALSE THEN
+        RAISE_APPLICATION_ERROR(-20005, 'El nombre del hospital no cumple con el patrón.');
+    END IF;
 
-        UPDATE Hospital
-        SET nombre = nombre_hospital, 
-            Antiguedad = antiguedad, 
-            Area = area, 
-            idSede = idSede, 
-            idGerente = idGerente, 
-            idCondicion = idCondicion, 
-            idDistrito = idDistrito, 
-            FechaRegistro = SYSDATE
-        WHERE idHospital = idHospital;
+    -- Actualizar la información del hospital
+    UPDATE Hospital
+    SET nombre = nuevo_nombre, 
+        Antiguedad = nueva_antiguedad, 
+        Area = nueva_area, 
+        FechaRegistro = SYSDATE
+    WHERE idHospital = idHospital;
 
-        COMMIT;
-    EXCEPTION
-        WHEN OTHERS THEN
-            ROLLBACK;
-            DBMS_OUTPUT.PUT_LINE('Ocurrió un error: ' || SQLERRM);
-    END SP_HOSPITAL_ACTUALIZAR;
+    COMMIT;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('No se encontró ningún hospital con el ID especificado.');
+    WHEN OTHERS THEN
+        ROLLBACK;
+        DBMS_OUTPUT.PUT_LINE('Ocurrió un error: ' || SQLERRM);
+END SP_HOSPITAL_ACTUALIZAR;
 
     PROCEDURE SP_HOSPITAL_ELIMINAR (
         idHospital IN INTEGER
@@ -170,23 +160,15 @@ CREATE OR REPLACE PACKAGE BODY HospitalPackage AS
             DBMS_OUTPUT.PUT_LINE('Ocurrió un error: ' || SQLERRM);
     END SP_HOSPITAL_ELIMINAR;
 
-     PROCEDURE SP_HOSPITAL_LISTAR IS
-        CURSOR hospital_cursor IS
-            SELECT H.* FROM Hospital H ORDER BY H.Antiguedad ASC;
-        hospital_record Hospital%ROWTYPE;
+    PROCEDURE SP_HOSPITAL_LISTAR IS
     BEGIN
-        OPEN hospital_cursor;
-        LOOP
-            FETCH hospital_cursor INTO hospital_record;
-            EXIT WHEN hospital_cursor%NOTFOUND;
-            DBMS_OUTPUT.PUT_LINE('ID: ' || hospital_record.idHospital || ', Nombre: ' || hospital_record.nombre || ', Antigüedad: ' || hospital_record.Antiguedad);
-        END LOOP;
-        CLOSE hospital_cursor;
+    FOR hospital_record IN (SELECT idHospital, nombre, Antiguedad FROM Hospital ORDER BY Antiguedad DESC) LOOP
+        DBMS_OUTPUT.PUT_LINE('ID: ' || hospital_record.idHospital || ', Nombre: ' || hospital_record.nombre || ', Antigüedad: ' || hospital_record.Antiguedad);
+    END LOOP;
     EXCEPTION
         WHEN OTHERS THEN
-            DBMS_OUTPUT.PUT_LINE('Ocurrió un error: ' || SQLERRM);
+        DBMS_OUTPUT.PUT_LINE('Ocurrió un error: ' || SQLERRM);
     END SP_HOSPITAL_LISTAR;
-
 END HospitalPackage;
 
 select h.*from hospital h;
@@ -205,16 +187,17 @@ BEGIN
 END;
 ----actualizar
 DECLARE
-    idHospital INTEGER := 1;
-    nombre_hospital VARCHAR2(100) := 'Nuevo Nombre del Hospital';
-    antiguedad INTEGER := 15;
-    area DECIMAL(10,2) := 250.75;
-    idDistrito INTEGER := 2;
-    idSede INTEGER := 2;
-    idGerente INTEGER := 2;
-    idCondicion INTEGER := 2;
+    v_idHospital INTEGER := 1;
+    v_nuevoNombre VARCHAR2(100) := 'Hospitalprueba';
+    v_nuevaAntiguedad INTEGER := 15;
+    v_nuevaArea DECIMAL(10, 2) := 250.75;
 BEGIN
-    SP_HOSPITAL_ACTUALIZAR(idHospital, nombre_hospital, antiguedad, area, idDistrito, idSede, idGerente, idCondicion);
+    SP_HOSPITAL_ACTUALIZAR(
+        v_idHospital,
+       v_nuevoNombre,
+         v_nuevaAntiguedad,
+         v_nuevaArea
+    );
 END;
 -------------------eliminar
 DECLARE
@@ -223,7 +206,9 @@ BEGIN
     SP_HOSPITAL_ELIMINAR(idHospitalEliminar);
 END;
 --------------listar
+
 BEGIN
     -- Llamada al procedimiento SP_HOSPITAL_LISTAR
     SP_HOSPITAL_LISTAR;
+ 
 END;
